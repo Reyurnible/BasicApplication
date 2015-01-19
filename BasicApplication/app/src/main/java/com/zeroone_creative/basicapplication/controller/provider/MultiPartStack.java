d9126da078495f4e457029d1d1b4205208b84889
保坂 駿 <reyurnible@gmail.com>
2015-01-19 11:51:24 +0900

    Fix request and FontUtil

--- BasicApplication/app/src/main/java/com/zeroone_creative/basicapplication/controller/provider/MultiPartStack.java
+++ BasicApplication/app/src/main/java/com/zeroone_creative/basicapplication/controller/provider/MultiPartStack.java
@@ -13,6 +13,7 @@ import org.apache.http.conn.scheme.PlainSocketFactory;
 import org.apache.http.conn.scheme.Scheme;
 import org.apache.http.conn.scheme.SchemeRegistry;
 import org.apache.http.conn.ssl.SSLSocketFactory;
+import org.apache.http.conn.ssl.X509HostnameVerifier;
 import org.apache.http.impl.client.DefaultHttpClient;
 import org.apache.http.impl.conn.tsccm.ThreadSafeClientConnManager;
 import org.apache.http.params.HttpConnectionParams;
@@ -21,11 +22,14 @@ import org.apache.http.params.HttpParams;
 import java.io.IOException;
 import java.util.Map;
 
+import javax.net.ssl.HostnameVerifier;
+
 /**
  * Created by shunhosaka on 2015/01/12.
  */
 public class MultiPartStack extends HurlStack {
     private final static String HEADER_CONTENT_TYPE = "Content-Type";
+    private final HostnameVerifier hostnameVerifier = SSLSocketFactory.ALLOW_ALL_HOSTNAME_VERIFIER;
 
     @Override
     public HttpResponse performRequest(Request<?> request, Map<String, String> additionalHeaders)
@@ -40,16 +44,19 @@ public class MultiPartStack extends HurlStack {
         addHeaders(httpRequest, request.getHeaders());
         HttpParams httpParams = httpRequest.getParams();
         int timeoutMs = request.getTimeoutMs();
-        HttpConnectionParams.setConnectionTimeout(httpParams, 5000);
+        //HttpConnectionParams.setConnectionTimeout(httpParams, 5000);
+        HttpConnectionParams.setConnectionTimeout(httpParams, 20 * 1000);
         HttpConnectionParams.setSoTimeout(httpParams, timeoutMs);
+
+        SSLSocketFactory socketFactory = SSLSocketFactory.getSocketFactory();
+        socketFactory.setHostnameVerifier((X509HostnameVerifier) hostnameVerifier);
 
         SchemeRegistry registry = new SchemeRegistry();
         registry.register(new Scheme("http", new PlainSocketFactory(), 80));
-        registry.register(new Scheme("https", SSLSocketFactory.getSocketFactory(), 443));
+        registry.register(new Scheme("https", socketFactory, 443));
 
         ThreadSafeClientConnManager manager = new ThreadSafeClientConnManager(httpParams, registry);
         HttpClient httpClient = new DefaultHttpClient(manager, httpParams);
-
         return httpClient.execute(httpRequest);
     }
 
@@ -65,4 +72,5 @@ public class MultiPartStack extends HurlStack {
             httpRequest.setEntity(((MultipartRequest) request).getEntity());
         }
     }
+
 }
---
+++ BasicApplication/app/src/main/java/com/zeroone_creative/basicapplication/controller/util/FontUtil.java
@@ -0,0 +1,88 @@
+package com.zeroone_creative.basicapplication.controller.util;
+
+import android.content.Context;
+import android.content.res.AssetManager;
+import android.graphics.Typeface;
+import android.util.Log;
+
+import java.io.File;
+import java.io.FileOutputStream;
+import java.io.InputStream;
+import java.util.zip.ZipEntry;
+import java.util.zip.ZipInputStream;
+
+/**
+ * Created by shunhosaka on 2015/01/18.
+ */
+public class FontUtil {
+
+    /**
+     * assetにある.ttfか.zipのファイルからフォントを取得する。
+     * パスはフルパス　font/フィアル名
+     * @param fontFilePath
+     * @param context
+     * @return
+     */
+    public static Typeface getFont(String fontFilePath, Context context) {
+        if (fontFilePath.contains(".ttf")) {
+            return getFontFromTtf(fontFilePath, context);
+        } else if (fontFilePath.contains(".zip")) {
+            return getFontFromZip(fontFilePath, context);
+        }
+        return null;
+    }
+
+    /**
+     * フォントファイルをassetから取得する。
+     * @param fontFileName
+     * @param context
+     * @return
+     */
+    private static Typeface getFontFromTtf(String fontFileName, Context context) {
+        AssetManager am = context.getAssets();
+        return Typeface.createFromAsset(am, fontFileName);
+    }
+
+    /**
+     * zip圧縮されているフォントファイルをassetから解凍し、dataフォルダに格納する
+     * assetのファイルはある程度大きいとzip圧縮されていないとAndroidで認識してくれない
+     * @param fontFileName
+     * @param context
+     * @return
+     */
+    private static Typeface getFontFromZip(String fontFileName, Context context) {
+        Typeface ret = null;
+        File zipFile = null;
+        try {
+            AssetManager am = context.getAssets();
+            InputStream is = am.open(fontFileName, AssetManager.ACCESS_STREAMING);
+
+            ZipInputStream zis = new ZipInputStream(is);
+            ZipEntry ze = zis.getNextEntry();
+            if (ze != null) {
+                zipFile = new File(context.getFilesDir(), ze.getName());
+
+                //フォントがすでに解凍されていればなにもしない
+                if(zipFile.exists()){
+                    return Typeface.createFromFile(zipFile.getPath());
+                }
+                FileOutputStream fos = new FileOutputStream(zipFile, false);
+                byte[] buf = new byte[1024];
+                int size = 0;
+                while ((size = zis.read(buf, 0, buf.length)) > -1) {
+                    fos.write(buf, 0, size);
+                }
+                fos.close();
+                zis.closeEntry();
+                ret = Typeface.createFromFile(zipFile.getPath());
+            }
+            zis.close();
+        } catch (Exception e) {
+            Log.e("FontUtil", "font extract fail", e);
+            if (zipFile!=null && zipFile.exists())
+                zipFile.delete();
+        }
+        return ret;
+    }
+
+}
