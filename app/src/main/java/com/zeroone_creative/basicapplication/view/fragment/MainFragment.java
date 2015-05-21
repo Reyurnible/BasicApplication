ae5e7d965f333db20b64215f25e2eb63ea85958c
Shun Hosaka <reyurnible@gmail.com>
2015-05-22 01:00:19 +0900

    Fix style resource

--- app/src/main/java/com/zeroone_creative/basicapplication/view/fragment/MainFragment.java
+++ app/src/main/java/com/zeroone_creative/basicapplication/view/fragment/MainFragment.java
@@ -1,7 +1,10 @@
 package com.zeroone_creative.basicapplication.view.fragment;
 
+import android.app.DatePickerDialog;
 import android.app.Fragment;
 import android.content.Intent;
+import android.widget.DatePicker;
+import android.widget.Toast;
 
 import com.zeroone_creative.basicapplication.R;
 import com.zeroone_creative.basicapplication.view.activity.PagerActivity_;
@@ -28,6 +31,22 @@ public class MainFragment extends Fragment {
     void moveRecycler() {
         Intent intent = new Intent(getActivity().getApplicationContext(), RecyclerActivity_.class);
         startActivity(intent);
+    }
+
+    @Click(R.id.main_button_dialog)
+    void showDialog() {
+        DatePickerDialog datePickerDialog = new DatePickerDialog(getActivity(), new DatePickerDialog.OnDateSetListener() {
+            @Override
+            public void onDateSet(DatePicker view, int year, int monthOfYear, int dayOfMonth) {
+                Toast.makeText(getActivity(),
+                        String.valueOf(year) + "/" +
+                                String.valueOf(monthOfYear + 1) + "/" +
+                                String.valueOf(dayOfMonth),
+                        Toast.LENGTH_SHORT).show();
+            }
+        },
+        2015, 5, 4);
+        datePickerDialog.show();
     }
 
 }
