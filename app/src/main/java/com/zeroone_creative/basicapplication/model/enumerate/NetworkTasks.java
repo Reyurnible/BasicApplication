2239ae4d6d465c3ccc4c24662b0320901f69a787
Shun Hosaka <reyurnible@gmail.com>
2015-06-15 18:44:02 +0900

    NetworkTasksの強化

--- app/src/main/java/com/zeroone_creative/basicapplication/model/enumerate/NetworkTasks.java
+++ app/src/main/java/com/zeroone_creative/basicapplication/model/enumerate/NetworkTasks.java
@@ -1,14 +1,35 @@
 package com.zeroone_creative.basicapplication.model.enumerate;
 
-public enum NetworkTasks {
+import android.net.Uri;
 
+import com.android.volley.Request;
+
+public enum NetworkTasks {
+    GetSample(0, Request.Method.GET),
     ;
+
     public int id;
     //Request
     public int method;
 
-    private NetworkTasks(int id, int method) {
+    NetworkTasks(int id, int method) {
         this.id = id;
         this.method = method;
+    }
+
+    static private String baseUrl = "sample.net";
+
+    public static Uri.Builder getUri(final NetworkTasks tasks) {
+        Uri.Builder builder = new Uri.Builder();
+        builder.scheme("http");
+        builder.encodedAuthority(baseUrl);
+        switch (tasks) {
+            case GetSample: {
+                //叩く先のAPI
+                builder.path("/api/Sample");
+                break;
+            }
+        }
+        return builder;
     }
 }
